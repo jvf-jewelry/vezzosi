fe03268ea16c0cd1556ecd5e6a153b9442a468b9
montanam <montanarimhl@gmail.com>
2019-10-14 12:10:11 +0200

    Update e allineamento

--- www/js/index.js
+++ www/js/index.js
@@ -2239,6 +2239,7 @@ app.controller("JvfController", function($scope, $route, $rootScope, $http, $coo
     $location.path("/post_detail");
     if (ctl.post_pics.length > 1){
       $('.carousel').carousel();
+      $('.carousel').bcSwipe({ threshold: 50 });
     }
   }
 
@@ -2265,6 +2266,7 @@ app.controller("JvfController", function($scope, $route, $rootScope, $http, $coo
     $location.path("/product_detail");
     if (ctl.product_detail.photo_galleries.length > 1){
       $('.carousel').carousel();
+      $('.carousel').bcSwipe({ threshold: 50 });
     }
   };
   
@@ -2555,6 +2557,69 @@ app.config(function($translateProvider) {
     })
   ;
 });
+/**
+ * Bootstrap Carousel Swipe v1.1
+ *
+ * jQuery plugin to enable swipe gestures on Bootstrap 3 carousels.
+ * Examples and documentation: https://github.com/maaaaark/bcSwipe
+ *
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+  $.fn.bcSwipe = function(settings) {
+    var config = { threshold: 50 };
+    if (settings) {
+      $.extend(config, settings);
+    }
+
+    this.each(function() {
+      var stillMoving = false;
+      var start;
+
+      if ('ontouchstart' in document.documentElement) {
+        this.addEventListener('touchstart', onTouchStart, false);
+      }
+
+      function onTouchStart(e) {
+        if (e.touches.length == 1) {
+          start = e.touches[0].pageX;
+          stillMoving = true;
+          this.addEventListener('touchmove', onTouchMove, false);
+        }
+      }
+
+      function onTouchMove(e) {
+        if (stillMoving) {
+          var x = e.touches[0].pageX;
+          var difference = start - x;
+          if (Math.abs(difference) >= config.threshold) {
+            cancelTouch();
+            if(isCarouselEmpty($(this))) return;
+            if (difference > 0) {
+              $(this).carousel('next');
+            }
+            else {
+              $(this).carousel('prev');
+            }
+          }
+        }
+      }
+      
+      function isCarouselEmpty(carousel){
+    return carousel.find(".item.active").length < 1
+      }
+
+      function cancelTouch() {
+        this.removeEventListener('touchmove', onTouchMove);
+        start = null;
+        stillMoving = false;
+      }
+    });
+
+    return this;
+  };
+})(jQuery);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -2580,6 +2645,8 @@ app.config(function($translateProvider) {
 
 // 
 // ANGULAR
+
+
 
 
 
